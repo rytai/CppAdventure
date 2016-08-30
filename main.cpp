341b9a62e3790942d1698b26c888a6d388ae457f
Rytai <Rytai@SATELLITE3347>
2016-08-30 13:08:30 +0300

    Projekti luotu visual studiolla

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// CppAdventure.cpp : Defines the entry point for the console application.
+
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+void testi_printti(std::string argumentti) {
+	std::cout << argumentti;
+}
+
+int main()
+{
+	cout << "\nDerp\n\n";
+
+	testi_printti("yolo\n\n");
+
+	//return 0;
+}
